5c724faf595cff6c535a9481459c2368a957e265
Richa Madhupriya <richa.madhupriya@outlook.com>
2018-07-23 21:56:24 +0530

    Shopping Cart

---
+++ app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from './product/product.component';
+
+@Component({
+  selector: 'app-root',
+  template: '<router-outlet></router-outlet>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ product/product.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+title:string="pen drive";
+itemCount = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
